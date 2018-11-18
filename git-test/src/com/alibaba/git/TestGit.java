c3e9534466108eafcd273f8b42fbf7bf1e931627
aliweng <aliweng5158@163.com>
2018-11-18 23:43:32 +0800

    提交

---
+++ git-test/src/com/alibaba/git/TestGit.java
@@ -0,0 +1,22 @@
+package com.alibaba.git;
+
+/**
+ * @Author： ~Mr·Weng~
+ * @CreateTime： 2018年-11月-18日  16:51
+ * Look at the wine in the glass toast the rich first.
+ */
+
+public class TestGit {
+
+    public static void main(String[] args) {
+
+        System.out.println("aliweng5158");
+        System.out.println("aliweng5158");
+        System.out.println("aliweng5158");
+        System.out.println("aliweng5158");
+        System.out.println("aliweng5158");
+        System.out.println("aliweng5158");
+
+    }
+
+}
